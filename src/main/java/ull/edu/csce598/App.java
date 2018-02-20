76544af2ba10ef0b6cf86150b2cbe08c613adec4
Murali Krishna Pusala <muralipusala@gmail.com>
2018-02-20 11:10:20 -0600

    Working example

--- src/main/java/ull/edu/csce598/App.java
+++ src/main/java/ull/edu/csce598/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        WordCount wc = new WordCount();
+        wc.run("resources/input_text.txt");
     }
 }
